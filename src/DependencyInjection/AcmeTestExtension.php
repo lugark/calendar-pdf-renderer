41ccf18248a1142ed3a77d354e40dfe166141795
Mathias <black_ice@web.de>
2022-07-06 18:31:00 +0200

    acme bundle

---
+++ src/DependencyInjection/AcmeTestExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Acme\TestBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class AcmeTestExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
