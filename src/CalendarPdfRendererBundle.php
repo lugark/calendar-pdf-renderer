7e0db56ee2f339d16edd935ca7df9a25fc5aa9e0
Mathias <black_ice@web.de>
2022-07-06 18:43:44 +0200

    render bundle

---
+++ src/CalendarPdfRendererBundle.php
@@ -0,0 +1,8 @@
+<?php
+namespace Calendar\Pdf\RendererBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CalendarPdfRendererBundle extends Bundle
+{
+}
--- src/DependencyInjection/CalendarPdfRendererExtension.php
+++ src/DependencyInjection/CalendarPdfRendererExtension.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Acme\TestBundle\DependencyInjection;
+namespace Calendar\Pdf\RendererBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class AcmeTestExtension extends Extension
+class CalendarPdfRendererExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
